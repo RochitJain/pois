a1832b7cf9258e0065bf451f57d3e3e458349a3e
RochitJain <rochitjain5261@gmail.com>
2026-04-15 10:42:27 +0530

    db neon setup

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,18 @@
 const {Pool} = require('pg')
 
-const pool = new Pool({
-    max: 10,
-    user: 'postgres',
-    host: 'localhost',
-    database: 'pois',
-    password: 'postgres',
-    port: 5432
-})
+// const pool = new Pool({
+//     max: 10,
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'pois',
+//     password: 'postgres',
+//     port: 5432
+// })
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
 module.exports = pool
